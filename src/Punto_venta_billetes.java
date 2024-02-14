da9d94cf83b6f13fbaaabdb695671e6d8875ff9f
Daniel <dmartinrodriguezdequesada@safareyes.es>
2024-02-14 20:01:32 +0100

    menu de opciones

--- src/Punto_venta_billetes.java
+++ src/Punto_venta_billetes.java
@@ -6,10 +6,10 @@ public class Punto_venta_billetes {
         Scanner lenteja = new Scanner(System.in);
         int opcion = 0;
         while (opcion != 3) {
-            System.out.println("1.-Transformación fecha suministrada como parámetro");
-            System.out.println("2.-Generación de la hora de llegada");
-            System.out.println("3.-Generación automatizada de tabla de meses");
-            System.out.println("4.-Obtención de los días correspondientes de un mes");
+            System.out.println("1).Transformación fecha suministrada como parámetro");
+            System.out.println("2).Generación de la hora de llegada");
+            System.out.println("3).Generación automatizada de tabla de meses");
+            System.out.println("4).Obtención de los días correspondientes de un mes");
             System.out.print("Ingrese una opción: ");
             opcion = lenteja.nextInt();
             switch (opcion) {
