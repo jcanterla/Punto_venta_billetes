7af4e42749192b6555e3e7773c981225285f2e6a
Danielmrq2005 <148747187+Danielmrq2005@users.noreply.github.com>
2024-03-04 16:06:08 +0100

    Add files via upload

---
+++ src/Pilotprimeraparte.java
@@ -0,0 +1,6 @@
+public class Pilotprimeraparte {
+    public static void main(String[] args) {
+        interfaz_primera_parte inter = new interfaz_primera_parte();
+        inter.crearInterfaz();
+    }
+}
